54653ec42ab73f85edef368bfd52ca67956c2a09
Dmitriy U <ukolov.d.90@mail.ru>
2023-02-13 15:14:20 +0300

    sloved task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше
+//a=5;b= 7 -> max = 7, min = 5
+//a=2;b= 10 -> max = 10, min = 2
+//a=-9;b= -3 -> max = -3, min = -9
+
+Console.WriteLine("Введите число A ");
+string answerA = Console.ReadLine();
+int usernumberA = Convert.ToInt32(answerA);
+
+Console.WriteLine("Введите число B ");
+string answerB = Console.ReadLine();
+int usernumberB = Convert.ToInt32(answerB);
+
+if (usernumberA > usernumberB)
+{
+  Console.WriteLine($"max={usernumberA}, min={usernumberB}");
+}
+else if (usernumberA < usernumberB)
+{
+  Console.WriteLine($"max={usernumberB}, min={usernumberA}");
+}
