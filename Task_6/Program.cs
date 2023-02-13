9cac88197a62c65f8619faee484eac87549e08a1
Dmitriy U <ukolov.d.90@mail.ru>
2023-02-13 19:57:41 +0300

    add sloved Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится на два без остатка)
+
+Console.WriteLine("Введите число ");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+
+int digit3 = usernumber % 2;
+if (digit3 == 0)
+{
+  Console.WriteLine("Четное");
+}
+else 
+{
+  Console.WriteLine("Нечетное");
+}
