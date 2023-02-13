575c17d9c2e73fb545be80272e8416903c3128fd
Dmitriy U <ukolov.d.90@mail.ru>
2023-02-13 20:46:10 +0300

    sloved Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все четные счисла от 1 до N.
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+  for (int i = 1; i <= N; i = i+1)
+  {
+    int digit3 = i % 2;
+    if (digit3 == 0)
+       {
+       Console.WriteLine(i);
+       }      
+  }
+  
